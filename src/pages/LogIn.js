fc5b83a2a9e8a3030ffb52684a868f9488aa5ecf
Royal Crystal Airways <royalcrystalairways@System-six.local>
2020-10-21 14:55:38 +0100

    login-page

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import '../App.css';
+import ReactDOM from 'react-dom';
+import "../components/css/form.css"
 
-function LogIn() {
+class LogIn extends React.Component {
+  render() {
     return (
-        <div>Log In</div>
-    )
+        <>
+        <div style={{
+            display: "flex",
+        }}>
+            <div style={{}}><img src={require('../assets/images/driver.png')} /></div>
+            <form style={{
+              marginLeft: "30px"
+            }}>
+            <img style={{
+              marginTop: "10%"
+            }} src={require('../assets/images/logo.svg')}></img>
+                <h1 style={{
+                  marginTop: "10%"
+                }}>Welcome! Enter your number</h1>
+                <p></p>
+                <input
+                type="text"
+                />
+            </form>
+        </div>
+        </>
+    );
+  }
 }
+
 export default LogIn;
