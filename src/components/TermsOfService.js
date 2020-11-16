0448c561c7054fe21f95f1a00a2fbeb1e53fc20c
Odunmorayo Mayowa <mayowajohn533@gmail.com>
2020-11-16 15:29:27 +0100

    updating content

--- src/components/TermsOfService.js
+++ src/components/TermsOfService.js
@@ -1,6 +1,57 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import './css/TermsOfService.css';
+import PropTypes, { number } from 'prop-types';
+
+
+const Input = ({ type = number, placeholder="Enter your phone number", name, value, onChange, error }) => {
+    return ( 
+        <input 
+            className="enterNo"
+            type = {type}
+            placeholder = {placeholder}
+            name = {name}
+            value = {value}
+            onChange = {onChange}
+            autoComplete = "off"
+        />
+    )
+}
+
+const Checkbox = ({ type = "checkbox", name, value, onChange, }) => {
+    return ( 
+        <input 
+            className="checkbox"
+            type = {type}
+            name = {name}
+            value = {value}
+            onChange = {onChange}
+        />
+    )
+}
+
+
+Input.propTypes = {
+    type: PropTypes.string,
+    placeholder: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.oneOfType([
+        PropTypes.number.isRequired,
+        PropTypes.object
+    ]),
+    onChange: PropTypes.func.isRequired
+}
+
+Checkbox.propTypes = {
+    type: PropTypes,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.oneOfType([
+        PropTypes.number.isRequired,
+    ]),
+    onChange: PropTypes.func.isRequired
+}
+
+
 
 function TermsOfService() {
     return (
@@ -9,12 +60,14 @@ function TermsOfService() {
                 <p>drive and cash out big time!</p>
                 <p1>Everything in life is somewhere else, and you get there in a car.</p1>
                 <label>
-                    <input className="enterNo" type="text" name="phone number" placeholder="Enter Mobile Phone Number" />
+                    <Input />
+                    {/* <input className="enterNo" type="text" name="phone number" placeholder="Enter Mobile Phone Number" /> */}
                 </label>
                 <Button className="applyToDrive1" id="apply">
                     Apply to Drive
                 </Button>{' '}
-                <div className="checkbox"></div>
+                {/* <div className="checkbox"><Checkbox /></div> */}
+                    <Checkbox />
                 <div className="agreement">I agree to RoyalTrip's terms of Service.</div>
             </form>
         </div>
