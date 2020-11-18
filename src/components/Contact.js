860c1db6159c822022452090ff9956c71c04ee1e
Odunmorayo Mayowa <mayowajohn533@gmail.com>
2020-11-18 11:29:33 +0100

    updating content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,15 +10,15 @@ function Contact() {
       <h2>BMTC Block 9, F.A.A.N Complex, Murtala Mohammed International Airport, Ikeja, Lagos State, Nigeria.</h2>
       <div className="location-details">
         <img className="location-icon1" src={require("../assets/images/1.svg")} alt="location-icon1" />
-        <p className="location1">2783 Terry LaneOrlando, FL 32805 </p>
+        <p className="location1"> BMTC Block 9, F.A.A.N Complex, M.M.I.A, Ikeja, Lagos State, Nigeria.</p>
       </div>
       <div className="location-details">
         <img className="location-icon2" src={require("../assets/images/2.svg")} alt="location-icon2" />
-        <p className="location2">1800 - 456 - 75001800 - 456 - 7600</p>
+        <p className="location2">info@royaltrips.com.ng; </p>
       </div>
       <div className="location-details">
         <img className="location-icon3" src={require("../assets/images/3.svg")} alt="location-icon3" />
-        <p className="location3">robertprice@gmail.comnig saran22@gmail.com</p>
+        <p className="location3">support@royaltrips.com.ng</p>
       </div>
       <FreePass />
     </div>
