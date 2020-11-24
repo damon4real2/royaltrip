1fb09f7c62c3a8616b08af36adff1eaa059c2a73
Odunmorayo Mayowa <mayowajohn533@gmail.com>
2020-11-24 16:27:48 +0100

    updating content

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ function button() {
         <div className="button">
             <div class="container">
                 <div class="row d-flex justify-content-around">
-                    <Button href="https://forms.gle/uHkPzawq51gFprtA9" className="sign-up">Sign up to ride</Button>{' '}
+                    <Button  className="sign-up">Sign up to ride</Button>{' '}
                     <Button href="https://forms.gle/uHkPzawq51gFprtA9" className="apply">Apply to drive</Button>{' '}
                     <Button href="/" className="talk">Our Company</Button>{' '}
                     <Button href="/" className="help">Help Center</Button>{' '}
