95e6a328ea595f3daebee2fa4ad71546cafeeae6
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2020-12-02 09:24:39 +0100

    Landing Page

--- src/pages/FindADriver.js
+++ src/pages/FindADriver.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import NavbarFindADriver from '../components/Navbar/NavbarFindADriver';
-// import SidebarFindADriver from '../components/Sidebar/SidebarFindADriver';
-import HeroFindADriver from '../components/Hero/HeroFindADriver';
-import BorderSpacingFindADriver from '../components/BorderSpacing/BorderSpacingFindADriver';
-import FlexboxFindADriver from '../components/Flexbox/FlexboxFindADriver';
+// import React from 'react';
+// import NavbarFindADriver from '../components/Navbar/NavbarFindADriver';
+// // import SidebarFindADriver from '../components/Sidebar/SidebarFindADriver';
+// import HeroFindADriver from '../components/Hero/HeroFindADriver';
+// import BorderSpacingFindADriver from '../components/BorderSpacing/BorderSpacingFindADriver';
+// import FlexboxFindADriver from '../components/Flexbox/FlexboxFindADriver';
 
 
-function FindADriver() {
-    return (
-        <div className="FindADriver">
-            <NavbarFindADriver />
-            {/* <SidebarFindADriver /> */}
-            <HeroFindADriver />
-            <BorderSpacingFindADriver />
-            <FlexboxFindADriver />
-        </div>
-    )
-}
-export default FindADriver;
+// function FindADriver() {
+//     return (
+//         <div className="FindADriver">
+//             <NavbarFindADriver />
+//             {/* <SidebarFindADriver /> */}
+//             <HeroFindADriver />
+//             <BorderSpacingFindADriver />
+//             <FlexboxFindADriver />
+//         </div>
+//     )
+// }
+// export default FindADriver;
