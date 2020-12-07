cad2d1d67415bc5806effac94b6691570a0d5747
Odunmorayo Mayowa <mayowajohn533@gmail.com>
2020-12-07 14:34:15 +0100

    fixes

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -4,7 +4,7 @@ import NavbarFAQ from '../components/NavbarFAQ';
 import HeroFAQ from '../components/HeroFAQ';
 import FAQComponent from '../components/FAQComponent';
 import Stay from '../components/Stay'
-import FooterFAQ from '../components/FooterFAQ';
+// import FooterFAQ from '../components/FooterFAQ';
 
 function FAQ() {
     return (
@@ -14,7 +14,7 @@ function FAQ() {
             <HeroFAQ />
             <FAQComponent />
             <Stay />
-            <FooterFAQ />
+            {/* <FooterFAQ /> */}
         </div>
     )
 }
