36f476b3ac91ca45a7745efa18610f78b81f09e7
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2020-12-16 10:18:57 +0100

    Content changed

--- src/components/Hero/HeroWhyJoinUsPage.js
+++ src/components/Hero/HeroWhyJoinUsPage.js
@@ -6,7 +6,7 @@ function HeroWhyJoinUsPage() {
     return (
         <div className="col-12 HeroWhyJoinUsPage">
             <h1>welcome to royaltrip, we are excited to have you here.</h1>
-            <p>We Launched into the market to deliver a human-friendly platform as a response to contain demands in the e-hailing ecosystem.</p>
+            <p>We launched into the market to deliver a human-friendly platform as a response to contain demands in the e-hailing ecosystem.</p>
             <div className="d-flex justify-content-center">
                 <button className="find_more">find more</button>
             </div>
