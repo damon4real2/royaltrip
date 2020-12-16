20b67edfbb472611a095b201fed6ce6093c64a0a
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2020-12-16 10:40:12 +0100

    fixes

--- src/components/Corporate/CorporateWhyJoinUs.js
+++ src/components/Corporate/CorporateWhyJoinUs.js
@@ -10,23 +10,23 @@ function CorporateWhyJoinUs() {
           <div class="row">
             <div class="col-sm aaaa">
               <img className="d-flex justify-content-center" src={require("./assets/images/icon_a.png")} alt="flexbox" />
-              <p className="aaaa_details">ride bonus</p>
-              <p1>Unlock lots of cool bonuses when you ride with RoyalTrip.</p1>
+              <p>ride bonus</p>
+              <p1 className="d-flex justify-content-center">Unlock lots of cool bonuses when you ride with RoyalTrip.</p1>
             </div>
             <div class="col-sm aaaa">
-              <img className="" src={require("./assets/images/icon_b.png")} alt="flexbox" />
-              <p className="aaaa_details">corporate trip</p>
-              <p1>Forward ride invoice to your corporate email, or sync RoyalTrip to your corporate account to automatically expense your business rides.</p1>
+              <img className="d-flex justify-content-center" src={require("./assets/images/icon_b.png")} alt="flexbox" />
+              <p>corporate trip</p>
+              <p1 className="d-flex justify-content-center">Forward ride invoice to your corporate email, or sync RoyalTrip to your corporate account to automatically expense your business rides.</p1>
             </div>
             <div class="col-sm aaaa">
-            <img className="" src={require("./assets/images/icon_c.png")} alt="flexbox" />
-              <p className="aaaa_details">easy bill</p>
-              <p1>Weary of uploading invoices? Simply sync your corporate card to pay for business rides directly — zero paperwork needed.</p1>
+            <img className="d-flex justify-content-center" src={require("./assets/images/icon_c.png")} alt="flexbox" />
+              <p>easy bill</p>
+              <p1 className="d-flex justify-content-center">Weary of uploading invoices? Simply sync your corporate card to pay for business rides directly — zero paperwork needed.</p1>
             </div>
             <div class="col-sm aaaa">
-            <img className="" src={require("./assets/images/icon_d.png")} alt="flexbox" />
-              <p className="aaaa_details">invite coworkers</p>
-              <p1>Riding together is more fun, and worth the reward. Refer colleagues at work to use RoyalTrip and earn a bonus for your personal rides.</p1>
+            <img className="d-flex justify-content-center" src={require("./assets/images/icon_d.png")} alt="flexbox" />
+              <p>invite coworkers</p>
+              <p1 className="d-flex justify-content-center">Riding together is more fun, and worth the reward. Refer colleagues at work to use RoyalTrip and earn a bonus for your personal rides.</p1>
             </div>
           </div>
         </div>
--- src/components/Footer/FooterNew.js
+++ src/components/Footer/FooterNew.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import "./FooterNew.css";
 
 function FooterNew() {
@@ -6,68 +7,84 @@ function FooterNew() {
       <div className="container-fluid FooterNew">
           <div className="row">
             <div className="col-4 FooterImg">
-              <img className="" src={require("./assets/images/logo.svg")} alt="flexbox" /><br></br>
+              <Link to="/">
+                <img className="" src={require("./assets/images/logo.svg")} alt="flexbox" /><br></br>
+              </Link>
               <div className="app_store-google_play">
-                <img className="" src={require("./assets/images/app-store.svg")} alt="flexbox" />
-                <img className="" src={require("./assets/images/google-play.svg")} alt="flexbox" /><br></br>
+                 {/* eslint-disable-next-line */}
+                <a href="https://www.apple.com/ng/ios/app-store/" target="_blank"><img className="" src={require("./assets/images/app-store.svg")} alt="flexbox" /></a>
+                 {/* eslint-disable-next-line */}
+                <a href="https://play.google.com/store/apps" target="_blank"><img className="" src={require("./assets/images/google-play.svg")} alt="flexbox" /><br></br></a>
               </div>
               <div className="question_1">
                 <img src={require("./assets/images/question.png")} alt="flexbox" />
-                <p>help center</p>
+                <a href="/">help center</a>
               </div>
               <br></br>
               <div className="container social_link">
-                <div className="row">
-                  <div className="col">
-                    <img src={require("./assets/images/fb.png")} alt="flexbox" />
-                  </div>
-                  <div className="col">
-                    <img src={require("./assets/images/twit.png")} alt="flexbox" />
-                  </div>
-                  <div className="col">
-                    <img src={require("./assets/images/inst.png")} alt="flexbox" />
-                  </div>
-                  <div className="col">
-                    <img src={require("./assets/images/youT.png")} alt="flexbox" />
-                  </div>
-                  <div className="col">
-                    <img src={require("./assets/images/pint.png")} alt="flexbox" />
-                  </div>
+                <div class="d-flex flex-row">
+                <div class="p-2">
+                  {/* eslint-disable-next-line */}
+                  <a href='https://web.facebook.com/Royal-Trips-107283564520818' target='_blank' aria-label='Facebook'><img src={require("./assets/images/fb.png")} alt="flexbox" /></a>
                 </div>
+                <div class="p-2">
+                  {/* eslint-disable-next-line */}
+                  <a href='https://twitter.com/RoyalTrips3' target='_blank' aria-label='Twitter'><img src={require("./assets/images/twit.png")} alt="flexbox" /></a>
+                </div>
+                <div class="p-2">
+                  {/* eslint-disable-next-line */}
+                  <a href='https://www.instagram.com/royaltrips_ng/' target='_blank' aria-label='Instagram'><img src={require("./assets/images/inst.png")} alt="flexbox" /></a>
+                </div>
+                <div class="p-2">
+                  <img src={require("./assets/images/youT.png")} alt="flexbox" />
+                </div>
+                <div class="p-2">
+                  <img src={require("./assets/images/pint.png")} alt="flexbox" />
+                </div>
+              </div>
               </div>
             </div>
             <div className="col-8 FooterLinks">
               <div className="container">
                 <div className="row">
                   <div className="col">
-                    <h1>our company</h1>
-                    <a className="d-flex justify-content-center" href="/">about us</a>
-                    <a className="d-flex justify-content-center" href="/">contact us</a>
-                    <a className="d-flex justify-content-center" href="/">FAQ</a>
-                    <a className="d-flex justify-content-center" href="/">privacy policy</a>
-                  </div>
-                  <div className="col">
                     <h1>for riders</h1>
-                    <a className="d-flex justify-content-center" href="/">sign in</a>
-                    <a className="d-flex justify-content-center" href="/">sign up</a>
-                    <a className="d-flex justify-content-center" href="/">Find a driver</a>
-                    <a className="d-flex justify-content-center" href="/">cities</a>
+                      <div class="d-flex flex-column">
+                        <a className="p-2" href="/">sign in</a>
+                        <a className="p-2" href="/">sign up</a>
+                        <a className="p-2" href="/">find a driver</a>
+                        <a className="p-2" href="/">cities</a>
+                    </div>
                   </div>
                   <div className="col">
                     <h1>for drivers</h1>
-                    <a className="d-flex justify-content-center" href="/">sign in</a>
-                    <a className="d-flex justify-content-center" href="/">sign up</a>
-                    <a className="d-flex justify-content-center" href="/">why join us?</a>
-                    <a className="d-flex justify-content-center" href="/">cities</a>
+                    <div class="d-flex flex-column">
+                      <a className="p-2" href="/">sign in</a>
+                      <a className="p-2" href="/">sign up</a>
+                      <a className="p-2" href="/">why join us?</a>
+                      <a className="p-2" href="/">cities</a>
+                    </div>
+                  </div>
+                  <div className="col">
+                    <h1>our company</h1>
+                      <div class="d-flex flex-column">
+                      <a className="p-2" href="/">about us</a>
+                      <a className="p-2" href="/">contact us</a>
+                      <a className="p-2" href="/">FAQ</a>
+                      <a className="p-2" href="/">privacy policy</a>
+                    </div>
                   </div>
                   <div className="col">
                     <h1>help</h1>
-                    <a className="d-flex justify-content-center" href="/">forum</a>
-                    <a className="d-flex justify-content-center" href="/">blog</a>
-                    <a className="d-flex justify-content-center" href="/">talk to us</a>
-                    <a className="d-flex justify-content-center" href="/">help line</a>
+                    <div class="d-flex flex-column">
+                      <a className="p-2" href="/">forum</a>
+                      <a className="p-2" href="/">blog</a>
+                      <a className="p-2" href="/">talk to us</a>
+                      <a className="p-2" href="/">help line</a>
+                    </div>
                   </div>
                 </div>
+                <div className="website_rights">&copy; 2020, Royal Trip. All rights reserved.</div>
               </div>
             </div>
           </div>
