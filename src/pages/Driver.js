928d94d9864a660ca8dcb207c5230961a2669f74
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-01-02 06:46:46 +0100

    Hamburger tweak

--- src/pages/Driver.js
+++ src/pages/Driver.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import '../App.css';
 import NavbarLogIn from '../components/NavbarLogIn';
-// import SidebarDriver from '../components/SidebarDriver';
 import TermsOfService from '../components/TermsOfService';
 import GetGoingDriver from '../components/GetGoing/GetGoingDriver';
 import CarouselDriver from '../components/Carousel/CarouselDriver'
 import EarningsDriver from '../components/Earnings/EarningsDriver'
-// import Footer from '../components/Footer'
+import FooterNew from '../components/Footer/FooterNew'
 
 
 function Driver() {
     return (
         <div className="Driver">
             <NavbarLogIn />
-            {/* <SidebarDriver /> */}
             <TermsOfService />
             <GetGoingDriver />
             <CarouselDriver /> 
             <EarningsDriver /> 
-            {/* <Footer /> */}
+            <FooterNew />
 
         </div>
     )
