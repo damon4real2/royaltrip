687ff96cc449b400cfee24fd0043af969180d072
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-01-04 08:39:12 +0100

    Riders page mobile

--- src/components/Earnings/EarningsRider.js
+++ src/components/Earnings/EarningsRider.js
@@ -5,23 +5,23 @@ import "./Earnings.css";
 
 function EarningsRider() {
     return (
-        <div className="container EarningsRider">
-            <div className="d-flex justify-content-between">
-              <p>How does RoyalTrip work?</p>
-              <Link to="/SignUpToDrive">
-                <p1>Sign up to ride, and ride with class.</p1>
-              </Link>
-            </div>
-            <video width="100%" autoPlay="autoplay" loop="loop" muted className="classesVideo" >
-              <source src={sample} type='video/mp4' />
-              Your browser does not support the video tag.
-            </video>
-            <div className="d-flex text-align-left earnings-brief">
-              <h4>Royalty, Safety and Secured</h4><br></br>
-              <p className="p">See how Mariam, Tolu, Usman, Debbie and Emeka use RoyalTrip to ride and enjoy their ride!</p>
-            </div>
-            <div className="container-fluid earnings-quote">My excitement in riding with RoyalTrip is that I get to ride with class, meet cultured drivers and also get to interact with the drivers in a cultured manner!<br></br><br></br>— Debbie”</div>
-          </div>
+      <div className="container EarningsRider">
+      <div className="d-flex justify-content-between">
+        <p>How does RoyalTrip work?</p>
+        <Link to="/SignUpToDrive">
+          <p1 className="link">Sign up to ride, and ride with class.</p1>
+        </Link>
+      </div>
+      <video width="100%" autoPlay="autoplay" loop="loop" muted className="classesVideo1" >
+        <source src={sample} type='video/mp4' />
+        Your browser does not support the video tag.
+      </video>
+      <div className="d-flex text-align-left earnings-brief">
+        <h4>Earnings, Self reliance, and Support</h4><br></br>
+        <div className="testimonial">See how Mariam, Tolu, Usman, Debbie and Emeka use RoyalTrip to ride and enjoy their ride!</div>
+      </div>
+      <div className="container-fluid earnings-quote-driver">“My joy in this partnership is the benefit that comes with driving on RoyalTrip platform. Owning a home within 6 months, Access to hire purchase within 3 months is not something I get to hear, except with royaltrip drivers.”<br></br><br></br>— Seun”</div>
+    </div>
         
     );
 }
